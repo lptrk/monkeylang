e7797baee3bd2a9c613f8ea7263649ed8c804ce5
lptrk <lktpriv@gmail.com>
2024-08-24 11:12:46 +0200

    feat(interpretor): Implement basic tokens

---
+++ token/token.go
@@ -0,0 +1,29 @@
+package token
+
+type TokenType string // Maybe use int or byte instead of string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	ASSIGN = "="
+	PLUS   = "+"
+
+	COMMA     = ","
+	SEMICOLON = ";"
+	LPAREN    = "("
+	RPAREN    = ")"
+	LBRACE    = "{"
+	RBRACE    = "}"
+
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
